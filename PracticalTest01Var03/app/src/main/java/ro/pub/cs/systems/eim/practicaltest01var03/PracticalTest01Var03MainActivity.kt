35e10b55bedd2212020d5577baf18395aee7d386
Luca Rachiteanu <rachiteanuluca@gmail.com>
2024-11-12 08:41:28 +0200

    Update PracticalTest01Var03MainActivity.kt

--- PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.kt
+++ PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.kt
@@ -21,26 +21,5 @@ class PracticalTest01Var03MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_practical_test01_var03_main)
 
-        number1 = findViewById<View>(R.id.number1) as EditText
-        number2 = findViewById<View>(R.id.number2) as EditText
-        result = findViewById<View>(R.id.result) as TextView
-        addButton = findViewById<View>(R.id.add_button) as Button
-        subtractButton = findViewById<View>(R.id.substract_button) as Button
-        nextButton = findViewById<View>(R.id.next_button) as Button
-
-        addButton!!.setOnClickListener {
-            val num1 = number1!!.text.toString().toInt()
-            val num2 = number2!!.text.toString().toInt()
-            val sum = num1 + num2
-            result!!.text = "Rezultat: $sum"
-        }
-
-        subtractButton!!.setOnClickListener {
-            val num1 = number1!!.text.toString().toInt()
-            val num2 = number2!!.text.toString().toInt()
-            val diff = num1 - num2
-            result!!.text = "Rezultat: $diff"
-        }
-
     }
 }
